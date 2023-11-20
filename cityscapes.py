5c3ddfdc701fce5f7e085d80efee6cdb0dbe54fd
ClaudiaCuttano98 <s288057@studenti.polito.it>
2023-11-20 17:26:31 +0100

    Initial commit

---
+++ cityscapes.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+from torch.utils.data import Dataset
+# TODO
+
+
+class CityScapes(Dataset):
+    def __init__(self, mode):
+        super(CityScapes, self).__init__()
+        # TODO
+
+    def __getitem__(self, idx):
+        # TODO
+
+    def __len__(self):
+        # TODO
